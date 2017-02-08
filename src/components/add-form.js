319267c68dbb24f1548b82394b86001ee89fd8b8
Joe Turner <joe@oampo.co.uk>
2017-02-08 17:34:56 +0000

    Various small fixes.

--- src/components/add-form.js
+++ src/components/add-form.js
@@ -6,15 +6,13 @@ export default class AddForm extends React.Component {
         this.state = {
             editing: false
         }
-
-        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onSubmit(event) {
         event.preventDefault();
         const text = this.textInput.value.trim();
         if (text && this.props.onAdd) {
-            this.props.onAdd(this.textInput.value);
+            this.props.onAdd(text);
         }
         this.textInput.value = '';
     }
@@ -29,14 +27,14 @@ export default class AddForm extends React.Component {
         if (!this.state.editing) {
             return (
                 <div className="add-button"
-                onClick={() => this.setEditing(true)}>
+                    onClick={() => this.setEditing(true)}>
                     <a href="#">Add a {this.props.type}...</a>
                 </div>
             );
         }
 
         return (
-            <form className="card add-form" onSubmit={this.onSubmit}>
+            <form className="card add-form" onSubmit={(e) => this.onSubmit(e)}>
                 <input type="text" ref={input => this.textInput = input} />
                 <button>Add</button>
                 <button type="button" onClick={() => this.setEditing(false)}>
